4b1d834fc55d79fa55e1ba5f0654b24f9e1d62ef
Segun Adebayo <joseshegs@gmail.com>
2021-09-21 15:11:15 +0400

    chore: update size tests

--- packages/tiny-size/tests/index.test.ts
+++ packages/tiny-size/tests/index.test.ts
@@ -1,29 +1,21 @@
-import { Size } from "../src"
+import { aspectRatio, flip, fromRadius, fromSquare, resize, size } from "../src"
 
-test("should create a size", () => {
-  const size = Size.create(10, 5)
-  expect(size).toMatchObject({ width: 10, height: 5 })
+describe("tiny-size", () => {
+  test("should create a size", () => {
+    const s = size(10, 5)
+    expect(s).toMatchObject({ width: 10, height: 5 })
+    expect(aspectRatio(s)).toEqual(2)
+    expect(flip(s)).toMatchObject({ width: 5, height: 10 })
+  })
 
-  expect(size.longestSide).toBe(10)
-  expect(size.shortestSide).toBe(5)
+  test("should create size from radius", () => {
+    expect(fromRadius(4)).toMatchObject({ width: 8, height: 8 })
+    expect(fromSquare(10)).toMatchObject({ width: 10, height: 10 })
+  })
 
-  expect(size.aspectRatio).toEqual(2)
-  expect(size.flip()).toMatchObject({ width: 5, height: 10 })
-})
-
-test("should create size from radius", () => {
-  expect(Size.fromRadius(4)).toMatchObject({ width: 8, height: 8 })
-  expect(Size.square(10)).toMatchObject({ width: 10, height: 10 })
-})
-
-test("should update size", () => {
-  const size = Size.create(10, 5)
-  size.set({ width: 20, height: 10 })
-  expect(size).toMatchObject({ width: 20, height: 10 })
-})
-
-test("should update size with aspectRatio lock", () => {
-  const size = Size.create(10, 5)
-  size.set({ width: 20 }, true)
-  expect(size).toMatchObject({ width: 20, height: 10 })
+  test("should update size", () => {
+    const s = size(10, 5)
+    const val = resize(s, { width: 20 }, true)
+    expect(val).toMatchObject({ width: 20, height: 10 })
+  })
 })
